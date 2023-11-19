51acbfeba66af95508e332e23a5c5fc619bbdc96
Peter Adams <peter.saccoiaadams@gmail.com>
2023-11-18 19:41:57 -0500

    Adding Various Menu's and Menu Controls
    - Added a new scene called MainMenu which will be the initial scene on loading the game.
    - Added 3 buttons which control the game: New Game starts the player at level 1 with all 3 lives and 0 coins, Continue loads the LevelSelect scene where the playerprefs were last saved, and Quit Game exits the application.
    - Created a MainMenu script to control these 3 buttons and assign them to the canvas where we used the canvas as the object for OnClick and pulled the respective functions.
    - Used a For Loop and an array to pull all the player progression and set them to 0, therefore it would reset to 0 regardless of player level progress.
    - Created a menu for the game over screen using the same logic above.
    - Only thing different was the Restart() function used in the GameOver script which resets the coincount and pulls the Level Manager starting lives number so we stay consistent.
    - Created a PauseScreen script with a new UI which stops the game on the escape button.
    - The PauseGame function sets the timescale to 0, canMove to false, and pauses the music as well as sets the UI to active.
    - The resume is also bound to "Pause" which inverts all conditions above.
    - Finally, we do the same thing in the other functions that we do in the Game Over screen, except before we do that we set the timescale back to 1 otherwise the next scene will load with 0 time and remain stuck.
    - Created a custom Input using the InputManager called "Pause". this uses the primary positive button as escape, and the alternate button Joystick Button 7. This allows us to scale cross platform.

---
+++ Assets/Scripts/PauseScreen.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseScreen : MonoBehaviour
+{
+
+    public string levelSelect, mainMenu;
+    public GameObject thePauseScreen;
+
+    private LevelManager theLevelManager;
+    private PlayerController thePlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        theLevelManager = FindObjectOfType<LevelManager>();
+        thePlayer = FindObjectOfType<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Pause"))
+        {
+            if(Time.timeScale == 0f)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    public void PauseGame()
+    {
+        Time.timeScale = 0f;
+        thePauseScreen.SetActive(true);
+        thePlayer.canMove = false;
+        theLevelManager.levelMusic.Pause();
+    }
+
+    public void ResumeGame()
+    {
+        thePauseScreen.SetActive(false);
+        Time.timeScale = 1f;
+        thePlayer.canMove = true;
+        theLevelManager.levelMusic.Play();
+    }
+
+    public void LevelSelect()
+    {
+        PlayerPrefs.SetInt("PlayerLives", theLevelManager.currentLives);
+        PlayerPrefs.SetInt("CoinCount", theLevelManager.coinCount);
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(levelSelect);
+    }   
+    
+    public void QuitToMainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(mainMenu);
+    }
+}
